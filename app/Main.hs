31cbb79b8ada17194ddc1bafe40b11c88fda1ebe
plavelo <plastic.velocity@gmail.com>
2019-08-24 10:17:13 +0900

    Atom, String, Numberのパーサーを追加
    Co-authored-by: spinylobster <spinylobster@outlook.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,48 @@
-import Text.Parsec
+import Text.Parsec hiding (spaces)
 import Text.Parsec.String
 import System.Environment
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             deriving (Show)
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Right val -> "Found value: " ++ show val
 
 main :: IO ()
 main = do
