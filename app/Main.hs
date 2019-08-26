18f9f33b9d2bde8bf4d3064abedbfdba8b531d63
plavelo <plastic.velocity@gmail.com>
2019-08-26 20:10:18 +0900

    構文解析を実装
    Co-authored-by: spinylobster <spinylobster@outlook.com>

--- app/Main.hs
+++ app/Main.hs
@@ -38,11 +38,32 @@ parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
         <|> parseNumber
+        <|> parseQuoted
+        <|> do
+              char '('
+              x <- try parseList <|> parseDottedList
+              char ')'
+              return x
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value: " ++ show val
+
+parseList :: Parser LispVal
+parseList = List <$> sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
 
 main :: IO ()
 main = do
